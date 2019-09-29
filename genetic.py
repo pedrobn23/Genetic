9d0e11cc5c10d80d868906733a8a8921ece0722e
Pedro Bonilla <pedrobonillanadal@hotmail.com>
2019-09-29 23:48:19 +0200

    Update genetic.py

--- genetic.py
+++ genetic.py
@@ -2,8 +2,7 @@ from population_examples import *
 from math import math
 
 class Genetic(object, ABC):
-    def __init__(self, n_evaluaciones=15000):
-        self.n_evaluaciones=n_evaluaciones
+    def __init__(self):
         self.initialize_population()
 
     @abstractmethod
@@ -13,10 +12,10 @@ class Genetic(object, ABC):
     def local_search(self):
         pass
 
-    def run(self):
+    def run(self, n_evaluaciones=15000):
         i = 0
         #Fijar numero de cuentas
-        while i < self.n_evaluaciones:
+        while i < n_evaluaciones:
             i+=1
             self.population.advance_generation()
 
