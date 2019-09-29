4f7b8ff72381c5f7168fc2b13d4d2d0961c8d179
pedrobn23 <pedrobonillanadal@hotmail.com>
2019-09-29 23:39:54 +0200

    redistribute examples

--- population.py
+++ population.py
@@ -7,9 +7,10 @@ from abc import ABC
 Gene = collections.namedtuple('Gene', 'characteristics value')
 
 class Population(list, ABC):
-    def __init__(self, key, n_characteristics=10, population_size=30):
+    def __init__(self, key, mutation_key, n_characteristics=10, population_size=30):
         super().__init__()
         self.evaluation_function = key
+        self.mutation_function = mutation_key
         self.value_list = []
         for i in range(population_size):
             self.append(np.random.rand(n_characteristics))
@@ -20,23 +21,18 @@ class Population(list, ABC):
 
     @abstractmethod
     def advance_generation(self):
-        pass
+        raise NotImplementedError
 
     def mutate_element(self, sigma=0.3, element=None):
         if not self:
             raise Exception('Can\'t mutate an element on a empty list')
 
         element = np.random.randint(len(self))
-        mutation = np.random.randint(len(self[0]))
-        deviation =  np.random.normal(0.5, sigma)
-        mutated_characts = self[element].characteristics
-
-        a = mutated_characts[mutation] + deviation
-        a = 0 if a < 0 else a if a < 1 else 1  # normalize
-        mutated_characts[mutation] = a
+        mutated_characts = mutation_function(self[element])
 
         self.pop(element)
         self.append(mutated_characts)
+
 
     def append(self, element):
         x = Gene(charasteristics=element,
@@ -48,33 +44,3 @@ class Population(list, ABC):
             if self[mid].value < x.value: lo = mid+1
             else: hi = mid
         self.insert(lo,x)
-
-
-#Ejemplos de Uso
-class StationaryPopulation(Population):
-    def __init__(self, reproduction_operator, evaluation_key, n_characteristics=10, population_size=30):
-        super().__init__(key=evaluation_key, n_characteristics=10, population_size=30)
-        self.reproduction_operator = reproduction_operator
-
-    def advance_generation(self):
-        parents = self.get_parents()         # Generamos los padres e hijos
-        hijos = self.reproduction_operator(self[parents[0]], self[partens[1]])
-        for h in hijos: self.append(h)         # Añadimos a la poblacion
-        del self[:2]  # Quitamos los dos peores
-
-class GenerationalPopulation(Population):
-    def __init__(self, reproduction_operator, evaluation_key, p_repro = 0.7,
-                 n_characteristics=10, population_size=30):
-        super().__init__(key=evaluation_key, n_characteristics=10, population_size=30)
-
-        self.repro = reproduction_operator
-        self.pairs = math.floor(self.size*p_cruze/2)
-
-    def advance_generation(self):
-        # Generamos los padres e hijos
-        for i in range(pairs):
-            parents = self.get_parents()
-            hijos = self.reproduction_operator(self[parents[0]], self[partens[1]])
-            for h in hijos: self.append(h)         # Añadimos a la poblacion
-        del self[:pairs*2]
-
